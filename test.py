c33d2dddecf3e46c702ec4dfc4d9c9709abca0d9
apurva <apurvagup@gmail.com>
2023-09-22 20:21:26 +0530

    tests for all

---
+++ test.py
@@ -0,0 +1,8 @@
+from micrograd_python import Value
+from micrograd_python import Layer, Module
+
+# make dataset with known results
+
+# create network using desired framework
+# train, infer
+# train, quantize, infer quantized
